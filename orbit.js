b1a6715da7baab20353897d05890b82282511040
Glitch (hello-webpage) <none>
2018-10-01 18:39:58 +0000

    🌦🐬 Checkpoint ./orbit.js:1907949/2099

--- orbit.js
+++ orbit.js
@@ -4,23 +4,25 @@ var seed, maxTimesteps, ctx, canvasSize, canvas;
 
 // Custom variables
 var mass; // The thing the satellites orbit
-var satellites = [], numSatellites; // The things orbiting the mass
+var satellites = [], currentNumSatellites, maxNumSatellites; // The things orbiting the mass
+var leader; // The invisible orbiting thing that decides whether to poop out satellites
 
 function drawMetadata() {
    
 }
 
-function createSatellite() {
+function createSatellite(x, y) {
   
   var satellite = {
     size: 1,
+    active: true,
     opacity: randomBetween(4, 9) / 10,
     colour: randomNeonColour(),
     pos: {
-      x: 0,
-      y: randomBetween(canvasSize.height*0.25, canvasSize.height*0.75),
+      x: x,
+      y: y,
     },
-    colourVariation: randomBetween(2, 10),
+    fillColour: randomNeonColour()
   }
   return satellite;
 }
@@ -37,9 +39,24 @@ function setupMass() {
   };
 }
 
+function setupLeader() {
+  var distanceFromMass = randomBetween(220, 500);
+  leader = {
+    distanceFromMass: distanceFromMass,
+    speed: 1,
+    pos: {
+      x: distanceFromMass,
+      y: distanceFromMass,
+    },
+    size: 10,
+    fillColour: randomLightColour(),
+  }
+}
+
 function setupSatellites(seed, maxTimesteps, ctx, canvasSize, canvas) {
   
   setupMass();
+  setupLeader();
   
   seed = seed;
   maxTimesteps = maxTimesteps;
@@ -50,19 +67,25 @@ function setupSatellites(seed, maxTimesteps, ctx, canvasSize, canvas) {
   xSpeed = randomBetween(4, 12) / 2;
   maxYVariation = randomBetween(5, 30);
   
-  numSatellites = randomBetween(10,100);
+  maxNumSatellites = randomBetween(10,100);
+  currentNumSatellites = 0;
   
-  for ( var i = 0; i < numSatellites; i++ ) {
-    satellites.push(createSatellite());  
-  }
   
 }
 
 
 function updateSatellites(dt, currentTimestep, paused) {
   
-  for ( var i = 0; i < numSatellites; i++ ) {
-    
+  
+  for ( var i = 0; i < currentNumSatellites; i++ ) {
+    if ( satellites[i].active ) {
+      satellites[i].opacity -= 0.01;
+      satellites[i].size += 0.01;
+
+      if ( satellites[i].opacity < 0 ) {
+        satellites[i].active = false; 
+      }
+    }
   }  
   
 }
@@ -75,11 +98,27 @@ function drawMass() {
 }
 
 
+function drawLeader() {
+  drawCircle(ctx, leader.pos.x, leader.pos.y, 1, leader.size, leader.fillColour);
+  
+  if ( currentNumSatellites < maxNumSatellites ) {
+    if ( randomBetween(1, 10) == 5 ) {
+      createSatellite(leader.x, leader.y); 
+    }  
+  }
+  
+  
+}
+
 function drawSatellites(ctx) {
   
   drawMass();
-  for ( var i = 0; i < numSatellites; i++ ) {
-     
+  drawLeader(); // Only for debug
+  
+  for ( var i = 0; i < currentNumSatellites; i++ ) {
+    if ( satellites[i].active ) {
+       drawCircle(ctx, satellites[i].pos.x, satellites[i].pos.y, 1, satellites[i].size, satellites[i].fillColour);
+    }
   }
   
 }
